6925e439925b32dc39804ef726ec083c8108896a
Anurag Srivastav <developer.anuragsrivastav@gmail.com>
2025-03-19 16:47:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+$str="hellon World";
+$length=strlen($str);
+echo "length of given string is ".$length."<br>";
+
+
+$a=10;
+$b=5;
+
+$a=$a-$b;
+$b=$b+$a;
+
+echo $a."<br>";
+echo $b;
+
+
+// sql quiries ------------------
+
+// INSERT INTO `students` (`id`, `name`, `class`, `dob`) VALUES (NULL, 'priya', '7', '2025-03-02 16:25:23'), (NULL, 'mohan', '5', '2025-03-19 11:55:23.000000');
+
+// to show 
+// select * from students;
+
+// inserting new student 
+// INSERT INTO `students` (`id`, `name`, `class`, `dob`) VALUES (NULL, 'rakesh', '4', '2025-03-02 16:25:23');
+
+
+
+
+
+
+
+
+?>
